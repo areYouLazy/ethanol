3e2eb5190a3006cf51b535d69b350bb1d405cd29
Emil Pandocchi <emil@ghost-shell.etere.loc>
2024-08-12 17:42:42 +0200

    fix configuration import

--- config/config.go
+++ config/config.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"path"
-
 	"github.com/areYouLazy/ethanol/flags"
 	"github.com/fsnotify/fsnotify"
 	"github.com/sirupsen/logrus"
@@ -10,28 +8,12 @@ import (
 )
 
 func Init() {
-	// check if there's a relative or absolute path in filename
-	fpath, fname := path.Split(flags.Config)
-
-	// path.Split() returns empty fpath if there are no slashes in the provided file path
-	if fpath == "" {
-		// add current path if omitted
-		fpath = "."
-	}
-
-	// add configuration path
-	viper.AddConfigPath(fpath)
-
 	// set filename
-	viper.SetConfigFile(fname)
-
-	// set configuration type, default to yaml
-	viper.SetConfigType("yaml")
+	viper.SetConfigFile(flags.Config)
 
 	// log
 	logrus.WithFields(logrus.Fields{
 		"file_name": viper.GetViper().ConfigFileUsed(),
-		"file_path": fpath,
 	}).Info("reading configuration file")
 
 	// read configuration file
